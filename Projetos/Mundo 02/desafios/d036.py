a5532202fe1722abc9e1989db37762d5eb91f48f
Gislaine Martins <gimcjf@gmail.com>
2024-06-20 09:30:38 -0300

    Resolução do desafio 36

--- Projetos/Mundo 02/desafios/d036.py
+++ Projetos/Mundo 02/desafios/d036.py
@@ -6,3 +6,16 @@ em quantos anos ele vai pagar.
 Calcule o valor da prestação mensal, sabendo que ela não pode exceder
 30% do salario ou então o emprestimo será negado
 """
+
+valor_casa = float(input('Qual o valor da casa: '))
+salario = float(input('Qual o seu salario: '))
+anos_pagamento = int(input('Em quanto anos vai pagar: '))
+
+anos_em_meses = anos_pagamento * 12
+limitacao_salario = salario * 0.3
+valor_mnesalidade = valor_casa / anos_em_meses
+
+if valor_mnesalidade < limitacao_salario:
+    print('Emprestimo AUTORIZADO! ')
+else:
+    print('Emprestimo NEGADO! ')
