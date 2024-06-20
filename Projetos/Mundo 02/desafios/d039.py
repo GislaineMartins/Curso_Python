e0a3bf2ae369c49d40927150e9164cdf3e45a8db
Gislaine Martins <gimcjf@gmail.com>
2024-06-20 09:46:28 -0300

    Resolução do desafio 39

--- Projetos/Mundo 02/desafios/d039.py
+++ Projetos/Mundo 02/desafios/d039.py
@@ -8,3 +8,18 @@ de acordo com sua idade:
 Seu programa tambem deverá mostrar o tempo que falta  ou quanto tempo
 passou do prazo
 """
+from datetime import date
+
+ano_nascimento = int(input('Informe o ano de nascimento: '))
+ano_atual = date.today().year
+subtracao_ano = ano_atual - ano_nascimento
+
+if subtracao_ano < 18:
+    print('Sua idade é {}.Você ainda vai se alistar ao serviço militar'.format(subtracao_ano))
+elif subtracao_ano == 18:
+    print('Sua idade é {}.Chegou a hora de alistar ao serviço militar'.format(subtracao_ano))
+elif subtracao_ano > 18:
+    print('Sua idade é {}.Já passou da hora de alistar ao serviço militar'.format(subtracao_ano))
+
+
+
