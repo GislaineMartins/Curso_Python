c57e9c1364204a760f6f1d010021a332d20df5dc
Gislaine Martins <gimcjf@gmail.com>
2024-06-20 09:54:48 -0300

    Resolução do desafio 40

--- Projetos/Mundo 02/desafios/d040.py
+++ Projetos/Mundo 02/desafios/d040.py
@@ -6,3 +6,18 @@ mostrando uma mensagem no final, de acordo com a média atingida:
 - Média entre 5.0 e 6.9: RECUPERAÇÃO
 - Média 7.0 ou superior: APROVADO
 """
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+if media < 5.0:
+    print('Sua média foi {}. REPROVADO!'.format(media))
+elif (media >= 5) and (media < 7.0):
+    print('Sua média foi {}. RECUPERAÇÃO!'.format(media))
+elif media >= 7.0:
+    print('Sua média foi {}. APROVADO!'.format(media))
+
+
+
