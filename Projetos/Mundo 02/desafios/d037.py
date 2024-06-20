9a46260308dae9027de12b942555512787f5b2d4
Gislaine Martins <gimcjf@gmail.com>
2024-06-20 09:59:34 -0300

    Resolução do desafio 37

--- Projetos/Mundo 02/desafios/d037.py
+++ Projetos/Mundo 02/desafios/d037.py
@@ -5,3 +5,12 @@ o usuário escolher qual será a base de conversão:
 - 2 para octal
 - 3 para hexadecimal
 """
+
+numero = int(input('Escreva um numero inteiro de 1 a 3: '))
+
+if numero == 1:
+    print('Conversao para binario!')
+elif numero == 2:
+    print('Conversao para octal!')
+elif numero == 3:
+    print('Conversao para hexadecimal!')
