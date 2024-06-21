1f86a216e57cf6516c6194c723157ca7f9ecc3a0
Gislaine Martins <gimcjf@gmail.com>
2024-06-21 10:07:10 -0300

    Resolução exercicio 43

--- Projetos/Mundo 02/desafios/d043.py
+++ Projetos/Mundo 02/desafios/d043.py
@@ -8,3 +8,18 @@ seu IMC e mostre seu status, de acordo com a tabela abaixo:
 - 30 a´te 40: Obesidade
 - Acima de 40: Obesidade mórbida
 """
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite seu altura: '))
+imc = peso/(altura * altura)
+
+if imc < 18.5:
+    print('IMC: {}! Abaixo do peso'.format(imc))
+elif imc >= 18.5 and imc <= 25:
+    print('IMC: {}! Peso ideal'.format(imc))
+elif imc > 25 and imc <= 30:
+    print('IMC: {}! Sobrepeso'.format(imc))
+elif imc > 30 and imc <= 40:
+    print('IMC: {}! Obesidade'.format(imc))
+else:
+    print('IMC: {}! Obesidade mórbida'.format(imc))
+
