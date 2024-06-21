8b4dc2659e0011afd29cfc928a6fed9c9b744640
Gislaine Martins <gimcjf@gmail.com>
2024-06-21 10:31:44 -0300

    Resolução exercicio 42

--- Projetos/Mundo 02/desafios/d042.py
+++ Projetos/Mundo 02/desafios/d042.py
@@ -5,3 +5,14 @@ que tipo de triangulo será formado:
 - Isóceles: dois lados iguais
 - Escaleno: todos os lados diferentes
 """
+print('Digite os lados dos triangulos!')
+primeiro_lado = float(input('Digite o lado 1:'))
+segundo_lado = float(input('Digite o lado 2:'))
+terceiro_lado = float(input('Digite o lado 3:'))
+
+if primeiro_lado == segundo_lado and primeiro_lado == terceiro_lado and segundo_lado == terceiro_lado:
+    print('Equilatero: todos os lados iguais')
+elif primeiro_lado != segundo_lado and primeiro_lado != terceiro_lado and segundo_lado != terceiro_lado:
+    print('Escaleno: todos os lados diferentes')
+elif primeiro_lado == segundo_lado or primeiro_lado == terceiro_lado or segundo_lado == terceiro_lado:
+    print('Isóceles: dois lados iguais')
