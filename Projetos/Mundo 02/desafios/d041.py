4944424a693c13b96e14be084314063977e96aff
Gislaine Martins <gimcjf@gmail.com>
2024-06-21 09:47:04 -0300

    Resolução exercicio 41

--- Projetos/Mundo 02/desafios/d041.py
+++ Projetos/Mundo 02/desafios/d041.py
@@ -8,3 +8,22 @@ a idade:
 - Até 20 anos: SENIOR
 - Acima de 20 anos: MASTER
 """
+from datetime import date
+
+ano_nascimento = int(input('Digite o ano em que nasceu: '))
+ano_atual = date.today().year
+idade = ano_atual - ano_nascimento
+
+if idade <= 9:
+    print('Voce tem {} anos. Categoria: MIRIM'.format(idade))
+elif idade > 9 and idade <= 14:
+    print('Voce tem {} anos. Categoria: INFANTIL'.format(idade))
+elif idade > 14 and idade <= 19:
+    print('Voce tem {} anos. Categoria: JUNIOR'.format(idade))
+elif idade > 19 and idade <= 20:
+    print('Voce tem {} anos. Categoria: SENIOR'.format(idade))
+else:
+    print('Voce tem {} anos. Categoria: MASTER'.format(idade))
+
+
+
