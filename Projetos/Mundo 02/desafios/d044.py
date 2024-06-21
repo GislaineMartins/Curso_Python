c681eb59286962abe3cbf1d47ac46a90d3d07be8
Gislaine Martins <gimcjf@gmail.com>
2024-06-21 10:20:09 -0300

    Resolução exercicio 44

--- Projetos/Mundo 02/desafios/d044.py
+++ Projetos/Mundo 02/desafios/d044.py
@@ -6,3 +6,25 @@ considerando o seu preço normal e condição de pagamento:
 - Em até 2x no cartão: preço normal
 - 3x ou mais no cartão: 20% de juros
 """
+
+valor_produto = float(input('Digite o valor do produto: '))
+print('Digite 1 para pagamento em (dinheiro/pix): ')
+print('Digite 2 para pagamento vista no cartão: ')
+print('Digite 3 para pagamento em até 2x no cartão: ')
+print('Digite 4 para pagamento 3x ou mais no cartão: ')
+opcao = int(input('Digite a opcao: '))
+
+if opcao == 1:
+    novo_valor = valor_produto - (valor_produto * 0.10)
+    print('10% de desconto. Valor R${}'.format(novo_valor))
+elif opcao == 2:
+    novo_valor = valor_produto - (valor_produto * 0.05)
+    print('5% de desconto. Valor R${}'.format(novo_valor))
+elif opcao == 3:
+    print('Não possui desconto. Valor R${}'.format(valor_produto))
+elif opcao == 4:
+    novo_valor = valor_produto + (valor_produto * 0.20)
+    print('Não possui desconto. Valor R${}'.format(novo_valor))
+else:
+    print('Opção invalida!')
+
