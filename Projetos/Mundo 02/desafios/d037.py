1f9c24a209c5d14fa6324beaa3d59ea9a8da876c
Gislaine Martins <gimcjf@gmail.com>
2024-06-24 09:14:15 -0300

    Correção exercicio 37

--- Projetos/Mundo 02/desafios/d037.py
+++ Projetos/Mundo 02/desafios/d037.py
@@ -6,11 +6,21 @@ o usuário escolher qual será a base de conversão:
 - 3 para hexadecimal
 """
 
-numero = int(input('Escreva um numero inteiro de 1 a 3: '))
+numero = int(input('Escreva um numero inteiro: '))
 
-if numero == 1:
-    print('Conversao para binario!')
-elif numero == 2:
-    print('Conversao para octal!')
-elif numero == 3:
-    print('Conversao para hexadecimal!')
+print(""" Escolha uma das bases para conversão:
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL
+""")
+opcao = int(input('Sua opção: '))
+
+if opcao == 1:
+    print('{} convertido para BINARIO é igal a {}'.format(numero, bin(numero)[2:]))
+elif opcao == 2:
+    print('{} convertido para OCTAL é igal a {}'.format(numero, oct(numero)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igal a {}'.format(numero, hex(numero)[2:]))
+else:
+    print('Opção invalida. Tente novamente')
+
