1bc2b84b08e8ae4798bd2dfbc2d1bc2de33d1d17
Gislaine Martins <gimcjf@gmail.com>
2024-06-27 09:38:54 -0300

    Resolução exercicio 49

--- Projetos/Mundo 02/desafios/d049.py
+++ Projetos/Mundo 02/desafios/d049.py
@@ -2,3 +2,9 @@
 Refaça o desafio 009(tabuada), mostrando a tabuada de um número que o usuário escolher, só que
 agora utilizando um laço for
 """
+
+n = int(input('Digite um numero: '))
+print('*********TABUADA*********')
+
+for i in range(1,n):
+    print('{} x {} = {}'.format(i,n, i*n))
