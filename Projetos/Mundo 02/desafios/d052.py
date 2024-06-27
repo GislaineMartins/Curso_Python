ee12194acb4350246b6e615f820ed8cf7c10fdb7
Gislaine Martins <gimcjf@gmail.com>
2024-06-27 09:52:28 -0300

    Resolução do Exercicio 52

--- Projetos/Mundo 02/desafios/d052.py
+++ Projetos/Mundo 02/desafios/d052.py
@@ -1,3 +1,10 @@
 """
 Faça um programa que leia um número inteiro e diga se ele é ou não um número inteiro
 """
+
+n = int(input('Digite um numero inteiro: '))
+for i in range(1, n+1):
+    if i.is_integer():
+        print('{} é numero inteiro'.format(i))
+    else:
+        print('{} NÃO é numero inteiro'.format(i))
