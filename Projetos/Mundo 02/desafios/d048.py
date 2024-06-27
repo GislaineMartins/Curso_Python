87f77a422bdc6102d037608b5421fd8f0658bc4c
Gislaine Martins <gimcjf@gmail.com>
2024-06-27 09:34:06 -0300

    Resolução do Exercicio 48

--- Projetos/Mundo 02/desafios/d048.py
+++ Projetos/Mundo 02/desafios/d048.py
@@ -2,3 +2,11 @@
 Faça um programa que calcule a soma entre todos os números impares que são multiplos de tres e que
 se encontram no intervalo de 1 até 500
 """
+soma = 0
+
+print('Soma de todos os número impares e multiplos de 3. No intervalo de 1 até 50')
+for i in range(1,501):
+    if i % 2 == 1 and i % 3 == 0:
+        soma += i
+        print(i)
+
