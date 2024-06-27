e0a89dfcd8d05074727bc3b6f57941af1963fdfa
Gislaine Martins <gimcjf@gmail.com>
2024-06-27 09:46:03 -0300

    Resolução do exercicio 50

--- Projetos/Mundo 02/desafios/d050.py
+++ Projetos/Mundo 02/desafios/d050.py
@@ -2,3 +2,10 @@
 Desenvolva um programa que leia 6 números inteiros e mostre a soma apenas daqueles que forem pares.
 Se o valor for impar desconsidere
 """
+soma = 0
+print('Digite 6 numeros!')
+for i in range(1,7):
+    n = int(input('Digite numero {}: '.format(i)))
+    if i % 2 == 0:
+        soma += i
+print('Soma de todos os numero pares {}'.format(soma))
